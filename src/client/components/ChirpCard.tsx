b24838ede6d907d8e1c3ef9968a87d0582b2c44f
plebtech <nbmc@protonmail.com>
2020-03-26 10:40:23 -0500

    'linked' chirp cards with onclick useHistory hook.

--- src/client/components/ChirpCard.tsx
+++ src/client/components/ChirpCard.tsx
@@ -1,13 +1,18 @@
 import * as React from 'react';
 import { IChirp } from '../utils/types';
+import { useHistory, Link } from 'react-router-dom';
 
 const ChirpCard: React.FC<ChirpCardProps> = props => {
-    return(
-        <div className="col-md-6 mx-1">
+
+    const history = useHistory();
+
+    return (
+        <div onClick={() => { history.push(`/details/${props.chirp.id}`)}} className="col-md-6 mx-1">
             <div className="card my-2 shadow">
                 <div className="card-body text-center">
                     <h4 className="card-title">@{props.chirp.username}</h4>
                     <p className="card-text">{props.chirp.content}</p>
+                    <Link to={`/details/${props.chirp.id}`}>get details.</Link>
                 </div>
             </div>
         </div>
--- src/client/views/Details.tsx
+++ src/client/views/Details.tsx
@@ -2,7 +2,9 @@ import * as React from 'react';
 import { useParams } from 'react-router-dom';
 
 const Details: React.FC<DetailsProps> = () => {
+
     const { id } = useParams();
+    
     return (
         <div>
             <h1 className="text-center">details view of {id}.</h1>
