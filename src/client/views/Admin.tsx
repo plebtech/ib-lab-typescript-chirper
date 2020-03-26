e905c2945820980932fb2f0e3ea9b1dcc74669e9
plebtech <nbmc@protonmail.com>
2020-03-26 09:50:38 -0500

    implementing id targeting for admin and details components.

--- src/client/views/Admin.tsx
+++ src/client/views/Admin.tsx
@@ -1,13 +1,15 @@
 import * as React from 'react';
+import { useParams } from 'react-router-dom';
 
 const Admin: React.FC<AdminProps> = () => {
+    const { id } = useParams();
     return (
         <div>
-            <h1 className="text-center">admin view.</h1>
+            <h1 className="text-center">admin view for {id}.</h1>
         </div>
     );
 };
 
-interface AdminProps {}
+interface AdminProps { }
 
 export default Admin;
--- src/client/views/Details.tsx
+++ src/client/views/Details.tsx
@@ -1,13 +1,15 @@
 import * as React from 'react';
+import { useParams } from 'react-router-dom';
 
 const Details: React.FC<DetailsProps> = () => {
+    const { id } = useParams();
     return (
         <div>
-            <h1 className="text-center">details view.</h1>
+            <h1 className="text-center">details view of {id}.</h1>
         </div>
     );
 };
 
-interface DetailsProps {}
+interface DetailsProps { }
 
 export default Details;
