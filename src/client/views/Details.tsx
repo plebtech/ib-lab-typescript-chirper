e44d1a900ffdcd4ac4127b031cd4a15b1acf99e7
plebtech <nbmc@protonmail.com>
2020-03-26 11:25:02 -0500

    expanded details component with useEffect, useState, useHistory.

--- src/client/views/Details.tsx
+++ src/client/views/Details.tsx
@@ -1,14 +1,44 @@
 import * as React from 'react';
-import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { useParams, useHistory, Link } from 'react-router-dom';
+import { IChirp } from '../utils/types';
 
 const Details: React.FC<DetailsProps> = () => {
 
     const { id } = useParams();
-    
+    const history = useHistory();
+
+    const [chirp, setChirp] = useState<IChirp>({
+        id: '',
+        username: '',
+        content: '',
+    });
+
+    useEffect(() => {
+        (async ()  => {
+            let res = await fetch(`/api/chirps/${id}`);
+            let chirp = await res.json();
+            setChirp(chirp);
+        })();
+    }, [id]);
+
     return (
-        <div>
-            <h1 className="text-center">details view of {id}.</h1>
-        </div>
+        <main className="container">
+            <section className="row my-2 justify-content-center">
+                <div className="col-md-12">
+                    <div className="card shadow">
+                        <div className="card-body text-center">
+                            <h4 className="card-title">@{chirp.username}</h4>
+                            <p className="card-text">{chirp.content}</p>
+                            <div className="d-flex justify-content-end align-items-center">
+                                <button onClick={() => history.goBack()} className="btn btn-outline-primary mx-1">go back.</button>
+                                <Link className="btn btn-outline-secondary mx-1" to={`/admin/${chirp.id}`}>edit.</Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
     );
 };
 
