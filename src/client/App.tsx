caaa343dd3a87b29a98df46a01700abf04334561
plebtech <nbmc@protonmail.com>
2020-03-26 09:45:32 -0500

    coded route paths in App switch.

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,15 +1,27 @@
 import * as React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './views/Home';
+import Details from './views/Details';
+import Admin from './views/Admin';
+import Compose from './views/Compose';
 
 const App: React.FC<AppProps> = () => {
 	return (
 		<BrowserRouter>
-		<Switch>
-			<Route exact path="/"></Route>
-			<Route exact path="/details/:id"></Route>
-			<Route exact path="/admin/:id"></Route>
-			<Route exact path="/compose"></Route>
-		</Switch>
+			<Switch>
+				<Route exact path="/">
+					<Home />
+				</Route>
+				<Route exact path="/details/:id">
+					<Details />
+				</Route>
+				<Route exact path="/admin/:id">
+					<Admin />
+				</Route>
+				<Route exact path="/compose">
+					<Compose />
+				</Route>
+			</Switch>
 		</BrowserRouter>
 	)
 }
---
+++ src/client/views/Compose.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+const Compose: React.FC<ComposeProps> = () => {
+    return (
+        <div>
+            <h1 className="text-center">compose view.</h1>
+        </div>
+    );
+};
+
+interface ComposeProps {}
+
+export default Compose;
