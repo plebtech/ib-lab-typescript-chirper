6ff4416099698ebad31ce32d437092064bf3808b
plebtech <nbmc@protonmail.com>
2020-03-26 09:37:20 -0500

    scaffolded Admin component.

---
+++ src/client/views/Admin.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+const Admin: React.FC<AdminProps> = () => {
+    return (
+        <div>
+            <h1 className="text-center">admin view.</h1>
+        </div>
+    );
+};
+
+interface AdminProps {}
+
+export default Admin;
